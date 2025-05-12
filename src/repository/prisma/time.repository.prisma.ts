ffc1e915a618bfc230912f1f04ad923e512ef2c3
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-05-12 14:03:41 -0300

    adding layer repository for entity time

---
+++ src/repository/prisma/time.repository.prisma.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import { Time } from "../../models/time";
+import { TimeRepository } from "../time.repository";
+
+
+
+export class TimeRepositoryPrisma implements TimeRepository {
+
+    constructor(private readonly prisma: PrismaClient){}
+
+    async create(time: Time): Promise<void> {
+        const createTime = await this.prisma.time.create({
+            data: {
+                id: time.id,
+                available: time.available,
+                date: time.date,
+                userId: time.userId,
+            }
+        })
+    };
+
+
+    async update(time: Time): Promise<void> {
+       const updateTime = await this.prisma.time.update({
+           where: {
+               id: time.id,
+           },
+           data: {
+               id: time.id,
+               available: time.available,
+               date: time.date,
+               userId: time.userId,
+           }
+       })
+    };
+
+    async finbyId(timeId: number): Promise<Time | null> {
+        const getTime = await this.prisma.time.findUnique({
+            where: {
+                id: timeId,
+            }
+        })
+        
+        if (!getTime) {
+            return null;
+        }
+        return Time.persistence(
+            getTime.id,
+            getTime.available,
+            getTime.date,
+            getTime.userId,
+        )
+    };
+
+    async delete(timeId: number): Promise<void> {
+       const deleteTime = await this.prisma.time.delete({
+           where: {
+               id: timeId
+           }
+       })
+    };
+}   
---
+++ src/repository/time.repository.ts
@@ -0,0 +1,8 @@
+import { Time } from "../models/time";
+
+export interface TimeRepository {
+    create(time: Time): Promise<void>;
+    update(time: Time): Promise<void>;
+    finbyId(timeId: number): Promise<Time | null>;
+    delete(timeId: number): Promise<void>;
+}
