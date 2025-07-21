44c30d1565e06d8d1b031d19cfa7692a9f3d1e53
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-07-21 14:39:59 -0300

    adicionando server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,42 @@
-import express from 'express'
 
-const app = express()
+import express from "express";
+import router from './routes/time.route';
+import barberRoutes from './routes/barber.route';
+import { TimeServiceIplement } from "./service/time.implementation";
+import { TimeRepositoryPrisma } from "./repository/prisma/time.repository.prisma";
+import  prisma  from "./repository/prisma/lib/conection";
 
-app.use(express.json())
+const timeRepository = new TimeRepositoryPrisma(prisma);
 
-app.get('/', (req, res) => {
-  res.send('Hello server')
-})
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3333, () => {
-  console.log('Server is running on http://localhost:3333')
-})
+ app.use('/api', router)
+ app.use('/api', barberRoutes)
+
+ app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+
+ app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+
+const timeService = TimeServiceIplement.getInstance(timeRepository);
+
+
+
+setInterval(() => {
+    console.log('🔄 Mantendo servidor ativo', new Date().toISOString());
+}, 300000);
+
+
+
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
