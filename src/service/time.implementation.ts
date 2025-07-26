11cffa13d958d91eb1e208f0e5b7a203e519f4da
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-07-26 09:45:54 -0300

    alternado horario

--- src/service/time.implementation.ts
+++ src/service/time.implementation.ts
@@ -51,7 +51,7 @@ export class TimeServiceIplement implements TimeService {
         // Mantém a instância ativa
         const keepAlive = setInterval(() => { }, 1000);
 
-        this.cronJob = cron.schedule('0 7 7 * * *', () => {
+        this.cronJob = cron.schedule('0 51 9 * * *', () => {
             console.log('⏰ Executando tarefa agendada independente');
             this.executeDailyTask()
                 .then(() => console.log('✅ Tarefa concluída'))
